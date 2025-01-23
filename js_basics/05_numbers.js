78cc973b92becd4e66becc57dd5a028febec10a2
Rishabh Sharma <146316487+StudentRishabhSharma@users.noreply.github.com>
2025-01-23 16:56:36 +0000

    done

---
+++ js_basics/05_numbers.js
@@ -0,0 +1,35 @@
+const num = 12
+console.log(num)
+
+const balance = new Number(150.4562)
+console.log(balance)
+
+console.log(num.toString)
+
+console.log(balance.toFixed(2))
+console.log(balance.toPrecision(4))
+
+
+//-----------MATH------------
+
+console.log(Math)
+console.log(Math.abs(-56))
+console.log(Math.round(256.55))
+console.log(Math.ceil(5.2))
+console.log(Math.floor(5.8))
+console.log(Math.min(2, 5, 4, 9))
+console.log(Math.max(2, 5, 4, 9))
+console.log(Math.sqrt(25))
+
+
+
+
+
+
+
+
+
+
+
+
+
