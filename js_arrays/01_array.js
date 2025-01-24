b1c8d49e4cbf538730428b3150d7c4538227061b
Rishabh Sharma <146316487+StudentRishabhSharma@users.noreply.github.com>
2025-01-24 13:07:29 +0000

    done

---
+++ js_arrays/01_array.js
@@ -0,0 +1,41 @@
+//array
+
+let arr = [0, 1, 2, true, "Rishabh"]
+
+
+
+// // Arrays Methods
+arr.pop()
+arr.pop()
+arr.push(3)
+arr.push(4)
+
+// arr.unshift(10)
+// arr.shift()
+
+// const newArr = arr.join()
+
+// console.log(arr);
+// console.log(newArr);
+console.log("-----------Slice---------");
+
+console.log("A ", arr);
+
+const na1 = arr.slice(1, 3)
+
+
+console.log("B ", na1);
+console.log("-----------Splice----------");
+
+const na2 = arr.splice(1, 3)
+console.log("A ", arr);
+console.log("B ", na2);
+
+
+
+
+
+
+
+
+
